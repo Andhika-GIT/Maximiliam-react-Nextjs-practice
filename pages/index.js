750b5e1c4bb6ab66e26258c0d8419e8072bcf32d
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-26 11:34:50 +0700

    setup the index render the MeetupList component with dummy meetups data

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,27 @@
 // our-domain.com/
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "M1",
+    title: "First meetup",
+    image:
+      "https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png",
+    address: "Ny city, something street",
+    description: "This is a first meetup",
+  },
+  {
+    id: "M2",
+    title: "Second meetup",
+    image:
+      "https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png",
+    address: "Chicago city, something street",
+    description: "This is a second meetup",
+  },
+];
+
+const homePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default homePage;
