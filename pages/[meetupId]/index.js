dbc38bdeb066ba365871960b8daca4c879be8bf3
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-27 14:13:02 +0700

    separate between component and pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,9 @@
 // our-domain.com/[meetupId] -> dynamic route params
 
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
 const MeetupDetails = () => {
-  return (
-    <>
-      <img src="https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png" alt="a first meetup" />
-      <h1>A first meetup</h1>
-      <address>Some street 5, some city</address>
-      <p>the meetup description</p>
-    </>
-  );
+  return <MeetupDetail image="https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png" title="first Meetup" address="some street 5, some city" description="this is first meetup" />;
 };
 
 export default MeetupDetails;
