00aca0b37208d033c7dfec4f30207e89c8e5c2b7
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-27 14:07:09 +0700

    adding some basic element to [meetupid] index.js

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,14 @@
 // our-domain.com/[meetupId] -> dynamic route params
+
+const MeetupDetails = () => {
+  return (
+    <>
+      <img src="https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png" alt="a first meetup" />
+      <h1>A first meetup</h1>
+      <address>Some street 5, some city</address>
+      <p>the meetup description</p>
+    </>
+  );
+};
+
+export default MeetupDetails;
