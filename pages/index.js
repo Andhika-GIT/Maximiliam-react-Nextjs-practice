92db19e20623406f2793fff3b5c24e96a0d50e7d
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-27 15:05:34 +0700

    the getStaticProps method this method will eventually get the fetch data before our component rendered so before the HomePage is rendered, we already got the meetup data list

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,35 @@
 // our-domain.com/
-import MeetupList from "../components/meetups/MeetupList";
+import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
   {
-    id: "M1",
-    title: "First meetup",
-    image:
-      "https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png",
-    address: "Ny city, something street",
-    description: "This is a first meetup",
+    id: 'M1',
+    title: 'First meetup',
+    image: 'https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png',
+    address: 'Ny city, something street',
+    description: 'This is a first meetup',
   },
   {
-    id: "M2",
-    title: "Second meetup",
-    image:
-      "https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png",
-    address: "Chicago city, something street",
-    description: "This is a second meetup",
+    id: 'M2',
+    title: 'Second meetup',
+    image: 'https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png',
+    address: 'Chicago city, something street',
+    description: 'This is a second meetup',
   },
 ];
 
-const homePage = () => {
+const HomePage = (props) => {
   return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
-export default homePage;
+export async function getStaticProps() {
+  // fetch data from an API
+
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
+
+export default HomePage;
