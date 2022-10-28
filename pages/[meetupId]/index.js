a36c7faaf252422daea80418c7cba4164f4695c6
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-28 19:49:44 +0700

    context paramater in getStaticProps we can get the params url like id by using context parameter in getStaticProps()

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -6,4 +6,25 @@ const MeetupDetails = () => {
   return <MeetupDetail image="https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png" title="first Meetup" address="some street 5, some city" description="this is first meetup" />;
 };
 
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  // get the id or the dynamic params in url using context.params
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image: 'https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png',
+        title: 'first Meetup',
+        address: 'some street 5, some city',
+        description: 'this is first meetup',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
