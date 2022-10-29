a1d345d4e3c41f0e1b37d370cd253ad9e933fa4d
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-29 15:17:06 +0700

    connect our app to mongodb server

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,6 +1,8 @@
 // this trigger when request send to -> /api/new-meetup
 
-const handler = (res, req) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (res, req) => {
   if (req.method === 'POST') {
     // if we receive post request, run below
 
@@ -9,6 +11,9 @@ const handler = (res, req) => {
 
     // destruct all the incoming request data
     const { title, image, address, description } = data;
+
+    // connect to our mongodb server
+    MongoClient.connect('mongodb+srv://Andhika:Lowjed026@cluster0.1ck4mzj.mongodb.net/meetups?retryWrites=true&w=majority');
   }
 };
 
