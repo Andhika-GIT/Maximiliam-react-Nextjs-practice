57e6c9fd5ea26fe7b84c0b2b5318041358d7c4ba
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-29 15:39:42 +0700

    trying to sending post request to our own api

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -4,7 +4,20 @@ import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 const NewMeetupPage = () => {
   const addMeetupHandler = async (enteredMeetupData) => {
     // sending request to our own api that we define in api folder
-    const response = await fetch('/api/new-meetup');
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      // send the meetup data
+      body: JSON.stringify(enteredMeetupData),
+      // to confirm that we're sending json data
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    // get the response
+    const data = await response.json();
+
+    console.log(data);
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
