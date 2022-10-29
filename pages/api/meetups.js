e371c179e8625be4be8f7eebcc2c238973fbbfdd
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-29 20:14:02 +0700

    creating another api files for get all meetups data

---
+++ pages/api/meetups.js
@@ -0,0 +1,27 @@
+// api/meetups
+
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
+  // connect to our mongodb server
+  const client = await MongoClient.connect('mongodb+srv://Andhika:MMmfGsbnaPpxPehq@cluster0.1ck4mzj.mongodb.net/?retryWrites=true&w=majority');
+
+  const db = client.db();
+
+  // get the collection ( collection -> like table in our table)
+  const meetupsCollection = db.collection('meetups');
+
+  // find or get the documents ( data ) in our collection ( table )
+  const meetups = await meetupsCollection.find().toArray();
+
+  console.log(meetups);
+
+  // close database connection
+  client.close();
+
+  // sending the respond after request
+  // define the status and sending json data ( like message and the data )
+  res.status(201).json({ message: 'meetup inserted!', meetups: meetups });
+};
+
+export default handler;
