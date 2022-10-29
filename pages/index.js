dfca1c6c0153bdf4ec0120fb3a7d4c0e5b1ba6a9
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-29 20:31:53 +0700

    remove the dummy meetups data

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,6 @@
 // our-domain.com/
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'M1',
-    title: 'First meetup',
-    image: 'https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png',
-    address: 'Ny city, something street',
-    description: 'This is a first meetup',
-  },
-  {
-    id: 'M2',
-    title: 'Second meetup',
-    image: 'https://www.objective.com/assets/content/images/Resources/Blog-Articles/img-blog-PNC-meetup.png',
-    address: 'Chicago city, something street',
-    description: 'This is a second meetup',
-  },
-];
-
 const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
