e3acb21d67992a768744f628f609fc6f19eb9dd9
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-29 15:35:34 +0700

    sending request to our own api endpoint

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,10 @@
 // our-domain.com/new-meetup
-import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+  const addMeetupHandler = async (enteredMeetupData) => {
+    // sending request to our own api that we define in api folder
+    const response = await fetch('/api/new-meetup');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
