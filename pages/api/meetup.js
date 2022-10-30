bd91cdc454d5f84e78e2af904dc13abef7e5acb2
Andhika Prasetya <andhikapr081@gmail.com>
2022-10-30 13:52:30 +0700

    preparing new api route for getting single meetup data the single meetup data is used when the user hover to meetup detail

---
+++ pages/api/meetup.js
@@ -0,0 +1,15 @@
+// this trigger when request send to ->  /api/meetups
+
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
+  // connect to our mongodb server
+  const client = await MongoClient.connect('mongodb+srv://Andhika:MMmfGsbnaPpxPehq@cluster0.1ck4mzj.mongodb.net/?retryWrites=true&w=majority');
+
+  const db = client.db();
+
+  // get the collection ( collection -> like table in our table)
+  const meetupsCollection = db.collection('meetups');
+};
+
+export default handler;
